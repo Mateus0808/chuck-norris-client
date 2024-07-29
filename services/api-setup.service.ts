dd1430d5035147d9e1177697101c5c42f9da0973
Mateus0808 <loiolamateus7@gmail.com>
2024-07-29 14:04:58 -0300

    env production

--- services/api-setup.service.ts
+++ services/api-setup.service.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-
 const apiSetup = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
